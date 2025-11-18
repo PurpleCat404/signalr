0a5d13359070b7edf3e344be01322ec541d93f04
PurpleCat404 <bednyak43@gmail.com>
2025-11-18 15:00:49 +0500

    1.1 done

---
+++ BadNews/Controllers/CommentsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using BadNews.Models.Comments;
+using BadNews.Repositories.Comments;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+public class CommentsController : ControllerBase
+{
+    private readonly CommentsRepository commentsRepository;
+
+    public CommentsController(CommentsRepository commentsRepository)
+    {
+        this.commentsRepository = commentsRepository;
+    }
+
+    // GET
+    [HttpGet("api/news/{id}/comments")]
+    public ActionResult<CommentsDto> GetCommentsForNews(Guid newsId)
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+}
