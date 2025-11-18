6140e08ee6702d433b8d580de19a83cd2e669a59
kitekatya <prostopostya221@yandex.ru>
2025-11-18 15:43:18 +0500

    add repo

--- BadNews/Controllers/CommentsController.cs
+++ BadNews/Controllers/CommentsController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using AutoMapper;
 using BadNews.Models.Comments;
 using BadNews.Repositories.Comments;
 using Microsoft.AspNetCore.Mvc;
@@ -7,18 +9,22 @@ using Microsoft.AspNetCore.Mvc;
 [ApiController]
 public class CommentsController : ControllerBase
 {
-    private readonly CommentsRepository commentsRepository;
+    private readonly ICommentsRepository commentsRepository;
+    private readonly IMapper mapper;
 
-    public CommentsController(CommentsRepository commentsRepository)
+    public CommentsController(ICommentsRepository commentsRepository, IMapper mapper)
     {
         this.commentsRepository = commentsRepository;
+        this.mapper = mapper;
     }
 
     // GET
     [HttpGet("api/news/{id}/comments")]
     public ActionResult<CommentsDto> GetCommentsForNews(Guid newsId)
     {
-        // TODO
-        throw new NotImplementedException();
+        var comments = commentsRepository.GetComments(newsId);
+        var dtoComments = mapper.Map<IReadOnlyCollection<CommentDto>>(comments);
+        var dto = new CommentsDto() { NewsId = newsId, Comments = dtoComments };
+        return dto;
     }
 }
---
+++ BadNews/Repositories/Comments/ICommentsRepository.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+namespace BadNews.Repositories.Comments;
+
+public interface ICommentsRepository
+{
+    IReadOnlyCollection<Comment> GetComments(Guid newsId);
+}
